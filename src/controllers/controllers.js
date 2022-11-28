3d92ae2ca29ca485fa600850e2e6ee7904ce75b0
David <david.cicconi94@gmail.com>
2022-11-28 13:15:09 -0300

    flights controllers ready

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -2,7 +2,7 @@ import { Airlines } from "../models/Airlines.js";
 import { Airports } from "../models/Airports.js";
 import { Flights } from "../models/Flights.js";
 
-// Airlines
+// AIRLINES
 export const getAirlines = async (req, res) => {
   try {
     const airlines = await Airlines.findAll();
@@ -183,8 +183,170 @@ export const getFlightsById = async (req, res) => {
   }
 };
 
-export const createFlight = async (req, res) => {};
+export const createFlight = async (req, res) => {
+  try {
+    const {
+      year,
+      month,
+      day,
+      day_of_week,
+      airline,
+      flight_number,
+      tail_number,
+      origin_airport,
+      destination_airport,
+      scheduled_departure,
+      departure_time,
+      departure_delay,
+      taxi_out,
+      wheels_off,
+      scheduled_time,
+      elapsed_time,
+      air_time,
+      distance,
+      wheels_on,
+      taxi_in,
+      scheduled_arrival,
+      arrival_time,
+      arrival_delay,
+      diverted,
+      cancelled,
+      cancellation_reason,
+      air_system_delay,
+      security_delay,
+      airline_delay,
+      late_aircraft_delay,
+      weather_delay,
+    } = req.body;
 
-export const updateFlight = async (req, res) => {};
+    const newFlight = Flights.create({
+      year,
+      month,
+      day,
+      day_of_week,
+      airline,
+      flight_number,
+      tail_number,
+      origin_airport,
+      destination_airport,
+      scheduled_departure,
+      departure_time,
+      departure_delay,
+      taxi_out,
+      wheels_off,
+      scheduled_time,
+      elapsed_time,
+      air_time,
+      distance,
+      wheels_on,
+      taxi_in,
+      scheduled_arrival,
+      arrival_time,
+      arrival_delay,
+      diverted,
+      cancelled,
+      cancellation_reason,
+      air_system_delay,
+      security_delay,
+      airline_delay,
+      late_aircraft_delay,
+      weather_delay,
+    });
+    res.json(newFlight);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
-export const deleteFlight = async (req, res) => {};
+export const updateFlight = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      year,
+      month,
+      day,
+      day_of_week,
+      airline,
+      flight_number,
+      tail_number,
+      origin_airport,
+      destination_airport,
+      scheduled_departure,
+      departure_time,
+      departure_delay,
+      taxi_out,
+      wheels_off,
+      scheduled_time,
+      elapsed_time,
+      air_time,
+      distance,
+      wheels_on,
+      taxi_in,
+      scheduled_arrival,
+      arrival_time,
+      arrival_delay,
+      diverted,
+      cancelled,
+      cancellation_reason,
+      air_system_delay,
+      security_delay,
+      airline_delay,
+      late_aircraft_delay,
+      weather_delay,
+    } = req.body;
+
+    const flight = await Flights.findByPk(id);
+
+    flight.year = year;
+    flight.month = month;
+    flight.day = day;
+    flight.day_of_week = day_of_week;
+    flight.airline = airline;
+    flight.flight_number = flight_number;
+    flight.yetail_numberar = tail_number;
+    flight.origin_airport = origin_airport;
+    flight.destination_airport = destination_airport;
+    flight.scheduled_departure = scheduled_departure;
+    flight.departure_time = departure_time;
+    flight.departure_delay = departure_delay;
+    flight.taxi_out = taxi_out;
+    flight.wheels_off = wheels_off;
+    flight.scheduled_time = scheduled_time;
+    flight.elapsed_time = elapsed_time;
+    flight.air_time = air_time;
+    flight.distance = distance;
+    flight.wheels_on = wheels_on;
+    flight.taxi_in = taxi_in;
+    flight.scheduled_arrival = scheduled_arrival;
+    flight.arrival_time = arrival_time;
+    flight.arrival_delay = arrival_delay;
+    flight.diverted = diverted;
+    flight.cancelled = cancelled;
+    flight.cancellation_reason = cancellation_reason;
+    flight.air_system_delay = air_system_delay;
+    flight.security_delay = security_delay;
+    flight.airline_delay = airline_delay;
+    flight.late_aircraft_delay = late_aircraft_delay;
+    flight.weather_delay = weather_delay;
+
+    await flight.save();
+    res.send("Updated");
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteFlight = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await Flights.destroy({
+      where: {
+        id,
+      },
+    });
+    res.send("Flight deleted");
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
